c04797b28a93c3c5d47b358eee87868785ba5c79
Rendy <rendy.de.p@gmail.com>
2020-04-24 21:15:37 +0700

    Add simple style for masthead

--- wp-content/themes/sklton/templates/global/masthead-open.php
+++ wp-content/themes/sklton/templates/global/masthead-open.php
@@ -4,7 +4,7 @@
  *
  * @author WPerfekt
  * @package Sklton
- * @version 0.0.2
+ * @version 0.0.3
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -12,7 +12,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 $masthead_class = sk_validate_var( $masthead_class, 'masthead' );
 $masthead_size  = sk_validate_var( $masthead_size, 'col-md-10 col-lg-8', false );
-$masthead_size .= ' mx-auto';
+$masthead_size .= ' mx-auto column-content d-flex h-100 justify-content-center align-items-center';
 ?>
 
 <section class="<?php echo esc_attr( $masthead_class ); ?>">
